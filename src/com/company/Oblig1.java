6b7325f1efef9bba6927d7532ae2a88c61c1dbea
Jesper <j.a.ostervold@gmail.com>
2020-09-09 17:41:36 +0200

    Replaced Norwegian letters.

--- src/com/company/Oblig1.java
+++ src/com/company/Oblig1.java
@@ -1,6 +1,6 @@
 package com.company;
 
-////// Løsningsforslag Oblig 1 ////////////////////////
+////// Losningsforslag Oblig 1 ////////////////////////
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,8 +16,8 @@ class Oblig1 {
             throw new NoSuchElementException("Arrayet er heelt tomt");
         }
         int i = 0;
-        // Går gjennom a og kjører så lenge i er kortere enn lengden til a,
-        // Indeksen er en mindre enn a.length så derfor står det a.length - 1
+        // Gar gjennom a og kjorer sa lenge i er kortere enn lengden til a,
+        // Indeksen er en mindre enn a.length sa derfor star det a.length - 1
         while (i < a.length - 1) {
             if (a[i] > a[i + 1]) {
                 int temp = a[i];
@@ -46,16 +46,16 @@ class Oblig1 {
     }
 
     /*
-    Når blir det flest ombyttinger?
-     - Når det største tallet står først blir det flest ombyttinger, siden den da må gå gjennom hele tabellen.
-    Når blir det færrest?
-     - Det blir færrest om arrayet er sortert stigende. Da blir det ingen.
+    Nar blir det flest ombyttinger?
+     - Nar det storste tallet star forst blir det flest ombyttinger, siden den da ma ga gjennom hele tabellen.
+    Nar blir det farrest?
+     - Det blir farrest om arrayet er sortert stigende. Da blir det ingen.
      Hvor mange blir det i gjennomsnitt?
       - (n-1 ganger siden den sammeligner i linje 35 og det blir den dominerende operasjonen.)
-      -  For eksempel med arraylengde 3 så blir gjennomsnittet 7/6
+      -  For eksempel med arraylengde 3 sa blir gjennomsnittet 7/6
 
-      Kan du på grunnlag av dette si om metoden maks er bedre ( eller dårligere ) enn
-      de maks-metodene vi har sett på tidligere?
+      Kan du pa grunnlag av dette si om metoden maks er bedre ( eller darligere ) enn
+      de maks-metodene vi har sett pa tidligere?
        -
 
 
@@ -106,7 +106,7 @@ class Oblig1 {
 
     ///// Oppgave 4 //////////////////////////////////////
 
-    // Lager en sortering type quicksort for å sortere arrayet
+    // Lager en sortering type quicksort for a sortere arrayet
     private static void sorter(int[] a, int start, int slutt) {
         if (start < slutt) {
             int pivot_indeks = partisjon(a, start, slutt);
@@ -141,8 +141,8 @@ class Oblig1 {
         int hoyre = n;
         int tellerOdd = 0;
         int tellerPar = 0;
-        // Sjekker om tallet er oddetall, og øker tellerOdd for hvert oddetall som man finner
-        // Sjekker så lenge venstre er kortere enn lengden til a som er definert som n
+        // Sjekker om tallet er oddetall, og oker tellerOdd for hvert oddetall som man finner
+        // Sjekker sa lenge venstre er kortere enn lengden til a som er definert som n
         while (venstre < hoyre) {
             while (a[venstre] % 2 != 0 && venstre < hoyre) {
                 venstre++;
@@ -159,21 +159,21 @@ class Oblig1 {
                 a[hoyre] = temp;
             }
         }
-        // Sorterer med quicksort fra første til n som er lengden på arrayet, hvis det kun er partall
+        // Sorterer med quicksort fra forste til n som er lengden pa arrayet, hvis det kun er partall
         if (tellerPar == 0) {
             sorter(a, 0, n);
         }
         // sorterer fra 0 til hvor mange partall det er
         sorter(a, 0, tellerOdd - 1);
-        // sorterer fra antall første oddetall til slutten av arrayet.
+        // sorterer fra antall forste oddetall til slutten av arrayet.
         sorter(a, tellerOdd, n);
     }
 
     ///// Oppgave 5 //////////////////////////////////////
-    // Endret denne til å returnere void, for å unngå warnings
+    // Endret denne til a returnere void, for a unnga warnings
     static void rotasjon(char[] a) {
-        for (int i = a.length - 1; i > 0; i--) { //Starter på slutten av array og jobber mot starten
-            char temp = a[i]; //Tar vare på verdien til a[i]
+        for (int i = a.length - 1; i > 0; i--) { //Starter pa slutten av array og jobber mot starten
+            char temp = a[i]; //Tar vare pa verdien til a[i]
             a[i] = a[i - 1];
             a[i - 1] = temp;
         }
@@ -253,9 +253,9 @@ class Oblig1 {
     static int[] indekssortering(int[] a) {
         // LAger et nytt array som er like langt som arrayet man skal sortere mtp indeksen.
         int[] arrayMedLengdeSomA = new int[a.length];
-        // kopierer a slik at man kan sortere uten å endre på a.
+        // kopierer a slik at man kan sortere uten a endre pa a.
         int[] kopiAvA = Arrays.copyOf(a, a.length);
-        // Benytter bubblesort for å løpe gjennom arrayet å finne
+        // Benytter bubblesort for a lope gjennom arrayet a finne
         for (int i = 0; i < kopiAvA.length; ++i) {
             for (int j = 0; j < kopiAvA.length - 1; ++j) {
                 if (kopiAvA[j] > kopiAvA[j + 1]) {
@@ -303,7 +303,7 @@ class Oblig1 {
         int tredjminstverdi = a[tm];
 
 
-        for (int i = 3; i < n; ++i) { //tre første er allerede sortert, starter på position 3
+        for (int i = 3; i < n; ++i) { //tre forste er allerede sortert, starter pa position 3
 
             int verdi = a[i];
             if (verdi < tredjminstverdi) {
@@ -337,7 +337,7 @@ class Oblig1 {
     /*static int bokstavNr(char bokstav) {
         throw new UnsupportedOperationException();
     }
-    Ser ikke ut til å trenge dette. Så fjerner for å unngå warnings.
+    Ser ikke ut til a trenge dette. Sa fjerner for a unnga warnings.
      */
 
     static boolean inneholdt(String a, String b) {
@@ -346,15 +346,15 @@ class Oblig1 {
         char[] bStringen = b.toCharArray();
         // Lager int array som man sammenligner
         int[] aString = new int[256], bString = new int[256];
-        // løper igjennom Astringen og legger det i aString
+        // loper igjennom Astringen og legger det i aString
         for (char c : aStringen) {
             aString[c]++;
         }
-        // løper igjennom bstringen og legger det i bString
+        // loper igjennom bstringen og legger det i bString
         for (char c : bStringen) {
             bString[c]++;
         }
-        // Løper igjennom hele int arrayet og sammenligner aString og bString
+        // Loper igjennom hele int arrayet og sammenligner aString og bString
         for (int i = 0; i < 256; ++i) {
             if (aString[i] > bString[i]) {
                 return false;
